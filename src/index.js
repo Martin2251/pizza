f35e8bc5e7e20d8bd43df40c2be50f0fa0813489
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-07-16 16:41:37 +0100

    with added props

--- src/index.js
+++ src/index.js
@@ -61,11 +61,23 @@ function Header () {
 function Menu (){
   return ( <main className="menu">
     <h2>Our menu</h2>
-    <Pizza  />
-    <Pizza  />
-    <Pizza  />
+    <Pizza name="Pizza Spinaci" ingredients="Tomato, mozarella, spinach and ricotta cheese" photoName="pizzas/spinaci.jpg" price="10"/>
+    <Pizza name="Pizza Funghi" ingredients="Tomato, mushrooms" price={13}photoName="pizzas/funghi.jpg" />
+ 
     </main>
   )
+}
+
+function Pizza(props){
+  console.log(props)
+  return <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+      <span>{props.price}</span>
+      </div>
+  </div>
 }
 
 function Footer () {
@@ -94,13 +106,7 @@ function App(){
 }
 
 
-function Pizza(){
-    return <div>
-        <img src="pizzas/spinaci.jpg" alt="pizza"  />
-        <h2>pizza spinaci</h2>
-        <p>Tomato, mozarella, spinach, and ricotta cheese</p>
-    </div>
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<React.StrictMode>
