503302b511442d7e9e297a64cc3fc728e22235e9
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-07-19 07:17:34 +0100

    ternarys

--- src/index.js
+++ src/index.js
@@ -64,11 +64,11 @@ function Menu (){
   return ( <main className="menu">
     <h2>Our menu</h2>
    
-    {numPizzas > 0  &&(
+    {numPizzas > 0 ?(
     <ul className="pizzas">{pizzas.map(pizza => <Pizza pizzaObj={pizza} key={pizza.name} />
 
     )}</ul>
-      )}
+      ):(<p>we are still woring on our menu please come back later</p>)}
     </main>
   )
 }
@@ -87,18 +87,19 @@ function Pizza(props){
 
 function Footer () {
   const hour =  new Date().getHours();
-  const openHour = 12;
+  const openHour = 20;
   const closeHour = 22;
   const isOpen = hour >= openHour && hour <=closeHour;
   console.log(isOpen);
 
   // if (hour >= openHour &&hour <=closeHour)alert("were currently open"); else alert("sorry we are closed")
   return <footer className="footer">
-    {isOpen &&
+    {isOpen ?(
     <div className="order">
      <p>were open unitl {closeHour}:00 </p>
      <button className="btn">Order</button>
      </div>
+    ):<p>we are happy to welcome you between {openHour}:00 and {closeHour}:00</p>
 }
   </footer>
 }
