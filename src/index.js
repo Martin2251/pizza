6c94f4326b67543f4cf1e1515ef9b438a32238b1
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-07-20 07:45:17 +0100

    finish up

--- src/index.js
+++ src/index.js
@@ -81,11 +81,13 @@ function Pizza({pizzaObj}){
 
   // if(pizzaObj.soldOut) return null;
   console.log(pizzaObj)
-  return <li className="pizza">
+  return <li className={`pizza ${pizzaObj.soldOut? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
       <h3>{pizzaObj.name}</h3>
       <p>{pizzaObj.ingredients}</p>
+
+
       <span>{ pizzaObj.soldOut ? "Sold Out" :pizzaObj.price}</span>
       </div>
   </li>
