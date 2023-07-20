e5f9759d7dcf648a82e7c7dca5c16dee036a72a4
Martin Lowe <59805940+Martin2251@users.noreply.github.com>
2023-07-20 07:15:46 +0100

    ternary again

--- src/index.js
+++ src/index.js
@@ -79,14 +79,14 @@ function Menu (){
 
 function Pizza({pizzaObj}){
 
-  if(pizzaObj.soldOut) return null;
+  // if(pizzaObj.soldOut) return null;
   console.log(pizzaObj)
   return <li className="pizza">
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
       <h3>{pizzaObj.name}</h3>
       <p>{pizzaObj.ingredients}</p>
-      <span>{pizzaObj.price}</span>
+      <span>{ pizzaObj.soldOut ? "Sold Out" :pizzaObj.price}</span>
       </div>
   </li>
 }
